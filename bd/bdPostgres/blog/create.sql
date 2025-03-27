8ea560f4b2e5da6d40c4b254f9a04aa758a40693
George Maxi <georgemaxi@Georges-Mac-mini.local>
2025-03-26 20:53:44 -0500

    insert

--- bd/bdPostgres/blog/create.sql
+++ bd/bdPostgres/blog/create.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS usuario(
 CREATE TABLE IF NOT EXISTS articulo(
     articulo_id SERIAL PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL, 
-    contenido TEXT NOT NULL
+    contenido TEXT NOT NULL,
     usuario_id_fk INT,
 
     FOREIGN KEY(usuario_id_fk) REFERENCES usuario(usuario_id)
@@ -24,3 +24,28 @@ CREATE TABLE IF NOT EXISTS comentario(
     FOREIGN KEY(articulo_id_fk) REFERENCES articulo(articulo_id)
 );
 
+CREATE TABLE IF NOT EXISTS categoria(
+    categoria_id SERIAL PRIMARY KEY,
+    nombre VARCHAR(255) UNIQUE NOT NULL,
+    descripcion VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS topics(
+    topic_id SERIAL PRIMARY KEY, 
+    nombre VARCHAR(255) NOT NULL,
+    descripcion VARCHAR(255),
+    categoria_id_fk INT,
+
+    FOREIGN KEY(categoria_id_fk) REFERENCES categoria(categoria_id)
+);
+
+CREATE TABLE IF NOT EXISTS articulo_categoria(
+    articulo_id_fk INT,
+    categoria_id_fk INT,
+    
+    FOREIGN KEY(articulo_id_fk) REFERENCES articulo(articulo_id),
+    FOREIGN KEY(categoria_id_fk) REFERENCES categoria(categoria_id)
+);
+
+ALTER TABLE categoria ADD CONSTRAINT order_unique UNIQUE (nombre);
+ALTER TABLE categoria ADD CONSTRAINT not_null UNIQUE (nombre);
---
+++ bd/bdPostgres/blog/insert.sql
@@ -0,0 +1,45 @@
+-- insercion categorias
+
+insert into categoria (nombre, descripcion) values ('Food - Produce', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.');
+insert into categoria (nombre, descripcion) values ('Food - Condiments', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.');
+insert into categoria (nombre, descripcion) values ('Food - Snacks', 'Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.');
+insert into categoria (nombre, descripcion) values ('Home', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.');
+insert into categoria (nombre, descripcion) values ('Audio', 'Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.');
+insert into categoria (nombre, descripcion) values ('Food - Dairy', 'Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.');
+insert into categoria (nombre, descripcion) values ('Food - Frozen Foods', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.');
+insert into categoria (nombre, descripcion) values ('Food - Sides', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.');
+insert into categoria (nombre, descripcion) values ('Clothing - Tops', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.');
+insert into categoria (nombre, descripcion) values ('Food - Prepared Foods', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.');
+
+
+-- insercion articulos
+
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Wolfsburg', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 98);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Enthusiasm (Entuziazm: Simfoniya Donbassa)', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 47);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('I Love You, Beth Cooper', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 1);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Help Me, Eros (Bang bang wo ai shen)', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 31);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Grey Fox, The', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.', 57);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Bush Mama', 'Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 4);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Windstruck (Nae yeojachingureul sogae habnida)', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 24);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Signos del zodiaco, Los', 'Phasellus in felis. Donec semper sapien a libero. Nam dui.', 100);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Flight of Fury', 'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 76);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Third Miracle, The', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.', 62);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Divergent', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 49);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Dans la peau d''une grande', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 66);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Henry Kissinger: Secrets of a Superpower (Henry Kissinger - Geheimnisse einer Supermacht)', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.', 30);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Child, The', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 20);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('In the House', 'Fusce consequat. Nulla nisl. Nunc nisl.', 96);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Love and Lemons (Små citroner gula)', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 76);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('The Snow White Murder Case', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.', 34);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Koch', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 87);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('An Amazing Couple', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.', 27);
+insert into articulo (titulo, contenido, usuario_id_fk) values ('Maelström', 'Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.', 50);
+
+
+-- relacion de articulo con categoria
+
+INSERT INTO articulo_categoria(articulo_id_fk, categoria_id_fk) VALUES (6, 4);
+INSERT INTO articulo_categoria(articulo_id_fk, categoria_id_fk) VALUES (6, 3);
+INSERT INTO articulo_categoria(articulo_id_fk, categoria_id_fk) VALUES (11, 3);
+
+SELECT * FROM articulo_categoria;
